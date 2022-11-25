1cd3138e8d1ed073a9cf2d5ca46b2ef72a616ae7
SpringNO26 <romain.tonneau@gmail.com>
2022-11-25 13:53:17 +0100

    first commit

---
+++ template/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier', '@typescript-eslint'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
+  rules: {
+    'react/jsx-uses-react': 1,
+    'arrow-body-style': ['error'],
+    'linebreak-style': 0,
+  },
+};
---
+++ template/src/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import Demo from '@components/Demo';
+
+const App = () => (
+  <View>
+    <Text> App</Text>
+    <Demo />
+  </View>
+);
+
+export default App;
